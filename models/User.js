e6cb59795ff19d071558e3dc8558dc18893e2847
Barto <solaribarto@gmail.com>
2026-09-13 03:06:13 -0300

    Borre nick uwu

--- models/User.js
+++ models/User.js
@@ -6,8 +6,7 @@ const userSchema = new Schema({
     xp: { type: Number, default: 0 },
     level: { type: Number, default: 0 },
     money: { type: Number, default: 0 },
-    rank: { type: String, default: "Miembro" },
-    minecraftNick: { type: String, default: null }
+    rank: { type: String, default: "Miembro" }
 }, { timestamps: true });
 
 module.exports = model('User', userSchema);
